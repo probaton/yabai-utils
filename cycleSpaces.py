992099aee070cdf55903baa6933c8dc9d6be2e81
vanderMandele, Dexx <dexx.vandermandele@homeadvisor.com>
2020-05-11 11:53:02 -0600

    Add query command and proper error handling

--- cycleSpaces.py
+++ cycleSpaces.py
@@ -1,11 +1,24 @@
 import subprocess
+import json
 
 def runCommand(command):
   process = subprocess.run(command.split(),
     stdout=subprocess.PIPE, 
     stderr=subprocess.PIPE,
     universal_newlines=True)
+  
+  if (process.stderr):
+    raise Exception(f"Command <{command}> failed:\n{process.stderr}")
+
   return process.stdout
 
+def runYabaiCommand(command):
+  yabaiCommand = "yabai -m " + command
+  return runCommand(yabaiCommand)
 
-print(runCommand('yabai -m query --displays'))
+def queryYabai(toQuery):
+  rawResult = runYabaiCommand("query " + toQuery)
+  return json.loads(rawResult)
+
+
+print(queryYabai("--displays"))
