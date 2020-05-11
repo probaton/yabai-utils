2cdb3f38c4603555f3627dd750652a870cb4feda
vanderMandele, Dexx <dexx.vandermandele@homeadvisor.com>
2020-05-11 12:56:31 -0600

    Cycle spaces, bitch!

--- cycleSpaces.py
+++ cycleSpaces.py
@@ -7,7 +7,7 @@ def runCommand(command):
     stderr=subprocess.PIPE,
     universal_newlines=True)
   
-  if (process.stderr):
+  if process.stderr:
     raise Exception(f"Command <{command}> failed:\n{process.stderr}")
 
   return process.stdout
@@ -20,5 +20,23 @@ def queryYabai(toQuery):
   rawResult = runYabaiCommand("query " + toQuery)
   return json.loads(rawResult)
 
+def queryYabaiIds(toQuery):
+  queryResult = queryYabai(toQuery)
+  if isinstance(queryResult, dict):
+    return queryResult["id"]
+  else: 
+    return list(map(lambda i: i["id"], queryResult))
 
-print(queryYabai("--displays"))
+def cycleSpaces():
+  currentSpace = queryYabai("--spaces --space")
+  displaySpaces = queryYabai("--spaces --display")
+  currentIndex = displaySpaces.index(currentSpace)
+
+  if currentIndex >= len(displaySpaces) - 1:
+    nextIndex = 0
+  else:
+    nextIndex = currentIndex + 1
+
+  runYabaiCommand(f"space --focus {displaySpaces[nextIndex]['index']}")
+
+cycleSpaces()
