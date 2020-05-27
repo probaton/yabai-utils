742c6cf644c6bec9cab8c8057a6181ee2dfe27d5
vanderMandele, Dexx <dexx.vandermandele@homeadvisor.com>
2020-05-27 11:20:21 -0600

    Add killSpace command

---
+++ killSpace.py
@@ -0,0 +1,13 @@
+from yabaiUtil import query, runCommand
+
+def killSpace():
+  currentSpace = query("--spaces --space")["index"]
+
+  winIds = map(lambda win: win["id"], query("--windows --space"))
+  for winId in winIds:
+    runCommand(f"window {winId} --close")
+  
+  runCommand(f"space --destroy {currentSpace}")
+
+if __name__ == "__main__":
+  killSpace()
